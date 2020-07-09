f45e60892dc845f5938616d5482fd7b1d46aad29
mamba2410 <callummcgregor54@gmail.com>
2020-07-09 17:29:34 +0100

    Updated arduino read sketch

--- src/arduino-spi-dump-eeprom/arduino-spi-dump-eeprom.ino
+++ src/arduino-spi-dump-eeprom/arduino-spi-dump-eeprom.ino
@@ -1,74 +1,71 @@
 #include <SPI.h>
 
-#define SPI_READ_CMD 0x03
 #define CS_PIN 10
+#define LED_INDICATOR 9
 
-#ifndef PIN_LED
-#define PIN_LED 13
-#endif
+#define SPI_READ_CMD 0x03
+#define SPI_TRANSFER_BYTE 0xff
+
+
+
+const unsigned short int EEPROM_START_ADDRESS   = 0x0000;
+const unsigned short int EEPROM_END_ADDRESS     = 0xc000;
+const unsigned short int NUM_BYTES              = (EEPROM_END_ADDRESS - EEPROM_START_ADDRESS);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(PIN_LED, OUTPUT);
+  pinMode(LED_INDICATOR, OUTPUT);
   pinMode(CS_PIN, OUTPUT);
   digitalWrite(CS_PIN, HIGH);
   SPI.begin();
 }
 
-void read_eeprom(unsigned int num_bytes) {
+void read_eeprom() {
   unsigned int addr;
   byte resp;
-  byte a, b, c, d;
+  byte a, b, c;
 
+  /* Start transmission */
   digitalWrite(CS_PIN, LOW);
 
-  /* transmit read command with 3 byte start address */
-  a = SPI.transfer(SPI_READ_CMD);
-  b = SPI.transfer(0x00);
-  c = SPI.transfer(0x00);
-  //SPI.transfer(0x00);
+  /* transmit read command with 2 byte start address */
+  a = SPI.transfer(SPI_READ_CMD);                     // Response from read
+  b = SPI.transfer(EEPROM_START_ADDRESS>>8);          // Shift in high byte of start address
+  c = SPI.transfer(EEPROM_START_ADDRESS&0xff);        // Shift in low byte of start address
 
-  Serial.print(a, HEX);
-  Serial.print(b, HEX);
+/*
+  Serial.print(a, HEX);           // Print the responses from the initial transfer
+  Serial.print(b, HEX); 
   Serial.print(c, HEX);
+*/
 
-  for (addr = 0; addr < num_bytes; addr++) {
-    resp = SPI.transfer(0xff);
+  for (addr = 0; addr < NUM_BYTES; addr++) {
+    resp = SPI.transfer(SPI_TRANSFER_BYTE);
+
+    /* Formatting */
     //if(!(addr%16)) Serial.print(" ");
     if(!(addr%32)) Serial.print("\n");
-    
-    //Serial.write(resp);
+
+    /* Print as ascii hex to serial */
     if(resp<0x10) Serial.print(0, HEX);
     Serial.print(resp, HEX);
 
     /* I got consistent results with a delay of 5ms*/
     delay(5);
   }
+  
+  /* End transmission */
   digitalWrite(CS_PIN, HIGH);
 }
 
-void dump() {
-  unsigned int num_bytes;
-  unsigned int i;
-
-  /* wait for the integer with the requested number of bytes */
-  while (Serial.available() < 4) {
-  }
-
-  /* ROM start sat 0x0050 and ends at 0xbfff */
-  num_bytes = 0xbfff - 0x0050 + 1;
-  //num_bytes = 1<<10;
-
-  read_eeprom(num_bytes);
-}
 
 void loop() {
+  digitalWrite(LED_INDICATOR, LOW);
 
-  digitalWrite(PIN_LED, LOW);
-
+  /* Wait for 'D' from serial to start reading */
   if (Serial.read() == 'D') {
-    digitalWrite(PIN_LED, HIGH);
-    dump();
+    digitalWrite(LED_INDICATOR, HIGH);
+    read_eeprom();
   }
 
 }
